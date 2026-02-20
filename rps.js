5327a24dc3bb716c9668ddad9ea05f586e26c94f
JSE955 <jordanscottedwards@gmail.com>
2026-02-20 11:24:47 -0500

    Declare player score variables

--- rps.js
+++ rps.js
@@ -1,8 +1,11 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
-    if (choice == 0) {
+    if (choice === 0) {
         return 'rock';
-    } else if (choice == 1) {
+    } else if (choice === 1) {
         return 'paper';
     } else {
         return 'scissors';
@@ -12,3 +15,4 @@ function getComputerChoice() {
 function getHumanChoice() {
     return prompt("Select rock, paper, or scissors");
 }
+
