c29a15efb506c0213f2f3c7089e1df512d45da66
JSE955 <jordanscottedwards@gmail.com>
2026-02-20 11:30:14 -0500

    Write the logic to play a single round

--- rps.js
+++ rps.js
@@ -16,3 +16,42 @@ function getHumanChoice() {
     return prompt("Select rock, paper, or scissors");
 }
 
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    if (humanChoice === "rock") {
+        if (computerChoice === "paper") {
+            console.log("You lose! Paper beats Rock!");
+            computerScore++;
+        } else if (computerChoice === "scissors") {
+            console.log("You win! Rock beats Scissors!");
+            humanScore++;
+        } else {
+            console.log("It's a tie!");
+        }
+    } else if (humanChoice === "paper") {
+        if (computerChoice === "scissors") {
+            console.log("You lose! Scissors beats Paper!");
+            computerScore++;
+        } else if (computerChoice === "rock") {
+            console.log("You win! Paper beats Rock!");
+            humanScore++;
+        } else {
+            console.log("It's a tie!");
+        }
+    } else if (humanChoice === "scissors") {
+        if (computerChoice === "rock") {
+            console.log("You lose! Rock beats Scissors!");
+            computerScore++;
+        } else if (computerChoice === "paper") {
+            console.log("You win! Scissors beats Paper!");
+            humanScore++;
+        } else {
+            console.log("It's a tie!");
+        }
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
